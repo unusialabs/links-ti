5315bb5a12a8010fa628f98426f843edcfa66994
irchamali <id.irchamali@gmail.com>
2023-05-10 09:57:22 +0700

    Update information and image

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,18 +7,19 @@ load_css()
 st.write("[![Star](https://img.shields.io/github/stars/irchamali.svg?logo=github&style=social)](https://github.com/irchamali)")
 
 col1, col2, col3 = st.columns(3)
-col2.image(Image.open('tiunusia.png'))
+col2.image(Image.open('img/tiunusia.png'))
 
-st.header('TI UNUSIA')
+st.header('Teknik Informatika')
 
-st.info('Program Studi Teknik Informatika Universitas Nahdlatul Ulama Indonesia')
+st.info('Program Studi Teknik Informatika Universitas Nahdlatul Ulama Indonesia (TI UNUSIA) menyelenggarakan pengajaran di kampus A Jakarta dan kampus B Bogor. Tersedia sejumlah beasiswa yang bisa diakses seperti beasiswa influencer, prestasi, NU leader future, hingga KIP Kuliah dari pemerintah.')
 
 icon_size = 20
 
 # st_button('linkedin', 'https://www.linkedin.com/in/ircham-ali/', 'Follow me on LinkedIn', icon_size)
-st_button('youtube', 'https://www.youtube.com/@unusialabs', 'YouTube', icon_size)
-# st_button('medium', 'https://irchamali.medium.com/', 'Read my Medium Blogs', icon_size)
-st_button('instagram', 'https://instagram.com/teknikinformatikaunusia/', 'Instagram', icon_size)
-st_button('twitter', 'https://twitter.com/teknikinformatikaunusia/', 'Twitter', icon_size)
-st_button('facebook', 'https://facebook.com/teknikinformatikaunusia/', 'Facebook', icon_size)
-st_button('github', 'https://github.com/unusialabs/', 'GitHub', icon_size)
+st_button('youtube', 'https://youtu.be/zKZt0GUluaI', 'Podcast x UnusiaTv', icon_size)
+st_button('youtube', 'https://www.youtube.com/@unusialabs', 'YouTube Channel', icon_size)
+st_button('medium', 'https://pmb.unusia.ac.id/', 'Yuk Join TI UNUSIA!', icon_size)
+st_button('facebook', 'https://facebook.com/teknikinformatikaunusia/', 'Facebook Laman', icon_size)
+st_button('instagram', 'https://instagram.com/teknikinformatikaunusia/', 'Liat Instagram', icon_size)
+st_button('github', 'https://github.com/unusialabs/', 'Repo GitHub', icon_size)
+st_button('twitter', 'https://twitter.com/ti_unusia/', 'Baca Twitter', icon_size)
